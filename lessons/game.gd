c44b5b23fd90513fb0cc011acbe8b5ccbc5f2358
xern0 <noblock@s.sfusd.edu>
2025-12-12 11:18:36 -0800

    Lesson 9 Completed

--- lessons/game.gd
+++ lessons/game.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-
+@onready var runner: Runner = $Runner
+@onready var count_down: CountDown = $CanvasLayer/CountDown
 @onready var finish_line: FinishLine = $FinishLine
 
 func _ready() -> void:
 	finish_line.body_entered.connect(func (body: Node) -> void:
 		if body is not Runner:
 			return
-		var runner := body as Runner
+		
 
 		runner.set_physics_process(false)
 		var destination_position := (
@@ -24,3 +25,9 @@ func _ready() -> void:
 	finish_line.confettis_finished.connect(
 		get_tree().reload_current_scene.call_deferred
 	)
+	count_down.start_counting()
+	runner.set_physics_process(false)
+	count_down.counting_finished.connect(
+		func() -> void:
+			runner.set_physics_process(true)
+)
