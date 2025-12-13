ea369408e98e6519ec52f116e651634d3329f55f
xern0 <noblock@s.sfusd.edu>
2025-12-13 00:09:17 -0800

    Lesson 13 Completed

--- lessons/bouncer.gd
+++ lessons/bouncer.gd
@@ -1,26 +1,37 @@
 extends CharacterBody2D
 
-## The top speed that the runner can achieve
+
 @export var max_speed := 600.0
-## How much speed is added per second when the player presses a movement key
+
 @export var acceleration := 1200.0
 
 @onready var dust: GPUParticles2D = %dust
 @onready var runner_visual: RunnerVisual = %RunnerVisualPurple
+@onready var hit_box: Area2D = %HitBox
+
+
+
+func _ready() -> void:
+	hit_box.body_entered.connect(func(body: Node) -> void:
+		if body is Runner:
+			get_tree().reload_current_scene.call_deferred()
+	)
 
 
 func _physics_process(delta: float) -> void:
 	var direction := global_position.direction_to(get_global_mouse_position())
 	var distance := global_position.distance_to(get_global_mouse_position())
+	var direction := global_position.direction_to(get_global_player_position())
+	var distance := global_position.distance_to(get_global_player_position())
 	var speed := max_speed if distance > 100 else max_speed * distance / 100
 
 	var desired_velocity := direction * speed
-
 	velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 	move_and_slide()
 
 	if velocity.length() > 10.0:
-		runner_visual.angle = rotate_toward(runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
+		var angle := rotate_toward(runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
+		runner_visual.angle = angle
 
 		var current_speed_percent := velocity.length() / max_speed
 		runner_visual.animation_name = (
--- lessons/game.gd
+++ lessons/game.gd
@@ -3,13 +3,14 @@ extends Node2D
 @onready var runner: Runner = $Runner
 @onready var count_down: CountDown = $CanvasLayer/CountDown
 @onready var finish_line: FinishLine = $FinishLine
+@onready var bouncer: CharacterBody2D = %Bouncer
 
 func _ready() -> void:
 	finish_line.body_entered.connect(func (body: Node) -> void:
 		if body is not Runner:
 			return
 		
-
+	
 		runner.set_physics_process(false)
 		var destination_position := (
 			finish_line.global_position
@@ -30,4 +31,11 @@ func _ready() -> void:
 	count_down.counting_finished.connect(
 		func() -> void:
 			runner.set_physics_process(true)
-)
+	)
+
+	bouncer.set_physics_process(false)
+
+	count_down.counting_finished.connect(
+		func() -> void:
+			bouncer.set_physics_process(true)
+	)
